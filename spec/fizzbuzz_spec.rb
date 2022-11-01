88cf24d8fcca34e6c306708d5585a221b5211482
Jude <ashworthjude@gmail.com>
2022-11-01 11:51:59 +0000

    Fizzbuzz pairing exercise

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,16 @@
-require 'fizzbuzz' describe 'fizzbuzz' do
+require 'fizzbuzz'
+
+describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" when passed a multiple of both 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'FizzBuzz'
+  end
   it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(3)).to eq 'Fizz'
+  end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
+  end
+  it 'returns number when passed a number that is not a multiple of 3 or 5' do
+    expect(fizzbuzz(1)).to eq 1
   end
 end
